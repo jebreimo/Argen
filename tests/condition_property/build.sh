93ded34601c02caa44c5dbc9a0f7a902934ddf8d
Jan Erik Breimo <jebreimo@gmail.com>
2013-11-17 14:01:35 +0100

    Changed header file extension to "hpp" in tests

--- tests/condition_property/build.sh
+++ tests/condition_property/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-clapgen --test "$DIR/helptext.txt" && \
+clapgen --test --hpp=hpp "$DIR/helptext.txt" && \
 c++ -std=c++11 -stdlib=libc++ "$DIR/ParseArguments.cpp" -o ParseArguments
