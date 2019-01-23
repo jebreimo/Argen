fe5f1ece2d9c924aa6bf7b0b578df3bb7a4e467a
Jan Erik Breimo <jebreimo@gmail.com>
2019-01-23 13:20:37 +0100

    Refined the help and final option deductions.

--- Argen/deduce_special_options.py
+++ Argen/deduce_special_options.py
@@ -20,12 +20,16 @@ def has_help_flags(flags):
 def find_best_help_options(arguments):
     candidates = {"callback": [], "flag": [], "type": []}
     for arg in arguments:
-        if arg.callback == "show_help":
+        if not arg.flags:
+            continue
+        elif arg.callback == "show_help":
             candidates["callback"].append(arg)
         elif arg.callback:
             continue
         elif arg.argument_type == "help":
             candidates["type"].append(arg)
+        elif arg.argument_type != "normal":
+            continue
         elif arg.callback is None and has_help_flags(arg.flags):
             candidates["flag"].append(arg)
     if candidates["type"]:
@@ -42,20 +46,25 @@ def find_best_help_options(arguments):
 
 def deduce_help_option(session):
     for arg in find_best_help_options(session.arguments):
-        arg.callback = "show_help"
-        if arg.post_operation is None:
-            arg.post_operation = "abort"
-            session.logger.debug("Deduced callback: %s and post-operation: %s."
-                                 % (arg.callback, arg.post_operation),
-                                 argument=arg)
-        else:
+        if arg.callback != "show_help":
+            arg.callback = "show_help"
             session.logger.debug("Deduced callback: %s." % arg.callback,
                                  argument=arg)
+        if arg.post_operation is None:
+            arg.post_operation = "abort"
+            session.logger.debug("Deduced post-operation: %s."
+                                 % arg.post_operation, argument=arg)
+        if arg.argument_type == "normal":
+            arg.argument_type = "help"
+            session.logger.debug("Deduced argument-type: %s."
+                                 % arg.argument_type, argument=arg)
 
 
 def looks_like_final_option(arg):
     if arg.argument_type == "final":
         return True
+    if arg.argument_type != "normal":
+        return False
     if not arg.flags or arg.flags[0] != "--" or len(arg.flags) != 1:
         return False
     return arg.operation in (None, "none")
@@ -64,9 +73,16 @@ def looks_like_final_option(arg):
 def deduce_final_option(session):
     for arg in session.arguments:
         if looks_like_final_option(arg):
-            arg.post_operation = "final"
-            arg.member_name = ""
-            session.logger.debug("Deduced final option.", argument=arg)
+            if arg.argument_type == "normal":
+                session.logger.debug("Deduced argument-type: final.",
+                                     argument=arg)
+            arg.argument_type = "final"
+            if arg.member_name is None:
+                arg.member_name = ""
+            if arg.post_operation != "final":
+                arg.post_operation = "final"
+                session.logger.debug("Deduced post-operation: final.",
+                                     argument=arg)
 
 
 def deduce_special_options(session):
